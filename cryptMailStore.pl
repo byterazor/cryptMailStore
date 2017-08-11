6ad37320de869481b3bfb9336f65f5aa057c3f96
Dominik Meyer <dmeyer@federationhq.de>
2017-08-11 22:01:37 +0200

    ADD: added pod

--- cryptMailStore.pl
+++ cryptMailStore.pl
@@ -1,3 +1,20 @@
 #!/usr/bin/perl -I lib
+
+=pod
+
+=head1 NAME
+
+CryptMailStore.pl - program to manage pgp encrypted mail stores
+
+=head1 AUTHOR
+
+Dominik Meyer <dmeyer@federationhq.de>
+
+=head1 LICENSE
+
+GPLv2
+
+=cut
+
  use CryptMailStore;
  CryptMailStore->new_with_command->run;
